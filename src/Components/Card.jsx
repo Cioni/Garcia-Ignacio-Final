76ac84a0bc03b588e0eaf8cb2cd73dbfed95e726
Cioni <nachii.cb.97@hotmail.com>
2023-06-26 22:30:44 -0300

    Refactor: se agrega libreria de iconos, los components y global context

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,23 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom/dist";
 import { useContextGobal } from "../Components/utils/global.context";
 
 const Card = ({ dentist }) => {
 
   const { name, username, id } = dentist
   const { state, dispatch } = useContextGobal()
+  const favs = state.favs
+  const isfav = favs.find
 
-  const addFav = ()=>{
+  const addFav = () => {
     // Aqui iria la logica para agregar la Card en el localStorage
+    dispatch({type: "addFav", payload: dentist})
+  }
+  const removeFav = () => {
+    dispatch({type:"removeFav", payload: dentist})
   }
 
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
-
+        <Link to={"/detail/" + id}>
+          <img src="public\images\doctor.jpg" alt=""/>
+          <h3>{name}</h3>
+          <p>{username}</p>
+        </Link>
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+          {!isfav
+            ? <button onClick={addFav} className="favButton"> Add Fav </button>
+            : <button onClick={removeFav} className="favButton"> Remove Fav </button>
+
+          }
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
+import { useContextGlobal } from './utils/global.context'
+import { Link } from 'react-router-dom/dist'
+import { FaBeer  } from 'react-icons/fa';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
+  const {state, dispatch} = useContextGlobal()
+  const changeTheme = () => {
+    !localStorage.getItem('theme') && localStorage.setItem('theme', state.theme)
+    localStorage.getItem('theme') === 'light'
+      ? dispatch({type: localStorage.getItem('theme'), payload: 'dark'})
+      : dispatch({type: localStorage.getItem('item'), payload: 'light'})
+  }
 
   return (
     <nav>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to='/'>
+        <h3 className='logo-nav'><span>Odonto</span></h3>  
+      </Link>
+      <div className='links'>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/favs'>Favs</Link>
+        <button onClick={changeTheme}
+        className='btn-theme'>
+          {state.theme === "dark"
+          ? <FaBeer />
+          : <FaBeer />
+          }
+        </button>
+      </div>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
     </nav>
   )
 }
